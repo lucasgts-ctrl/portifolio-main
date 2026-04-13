2e37ac3809c44163f3e616bc8c98194990d5debb
Lucas <lucagabrielsantos5@gmail.com>
2026-04-13 10:28:55 -0300

    updt

---
+++ portifolio-main/script.js
@@ -0,0 +1,11 @@
+function ativaLetra(elemento) {
+    const arrayTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrayTexto.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra;
+        }, 75 * i);
+    });
+}
+const titulo = document.querySelector('.digitando');
+ativaLetra(titulo);
